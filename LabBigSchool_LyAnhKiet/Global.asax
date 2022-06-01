680d3721eed6c3a0895d8038b61c12c7fc41460e
Admin <Admin@DESKTOP-7NHJLEF>
2022-06-01 16:06:38 +0700

    Add project files.

---
+++ LabBigSchool_LyAnhKiet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LabBigSchool_LyAnhKiet.MvcApplication" Language="C#" %>
---
+++ LabBigSchool_LyAnhKiet/Models/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LabBigSchool_LyAnhKiet.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        // Them domain class Course va Category
+        // ko DBset 2 thang duoi thi add-migration ko ra table dc
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}

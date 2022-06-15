d4997fc0dbf65c8176cc966971e929af32bc0121
anhkiet150700@gmail.com <anhkiet150700@gmail.com>
2022-06-15 16:05:57 +0700

    15/06

---
+++ LabBigSchool_LyAnhKiet/Controllers/AttendancesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Mvc;
+using System.Web.Http;
+using LabBigSchool_LyAnhKiet.Models;
+using Microsoft.AspNet.Identity;
+using LabBigSchool_LyAnhKiet.DTOs;
+
+namespace LabBigSchool_LyAnhKiet.Controllers
+{
+    [System.Web.Http.Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _dbContext;
+        public AttendancesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        [System.Web.Http.HttpPost]
+        public IHttpActionResult Attend(AttendanceDto attendanceDto)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Attendances.Any(a => a.AttendeeId == userId && a.CourseId ==attendanceDto.CourseId))
+                return BadRequest("The Attendance already exists!");
+            var attendance = new Attendance
+            {
+                CourseId = attendanceDto.CourseId,
+                AttendeeId = userId
+            };
+            _dbContext.Attendances.Add(attendance);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+    }
+}

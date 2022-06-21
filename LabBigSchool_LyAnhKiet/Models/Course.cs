e6a1e6fa92ff339a30e55a3a4da91bdd4b9c362f
anhkiet150700@gmail.com <anhkiet150700@gmail.com>
2022-06-22 00:02:44 +0700

    21/06

--- LabBigSchool_LyAnhKiet/Models/Course.cs
+++ LabBigSchool_LyAnhKiet/Models/Course.cs
@@ -9,6 +9,7 @@ namespace LabBigSchool_LyAnhKiet.Models
     public class Course
     {
         public int ID { get; set; }
+        public bool IsCanceled { get; set; }
         public ApplicationUser Lecturer { get; set; }
         [Required]
         public string LecturerId { get; set; }
